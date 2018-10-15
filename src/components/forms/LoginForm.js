a26c1d9785706958eb448548faa34b786af3b38a
Brandon Kandarapally <kandarapallybl@msoe.edu>
2018-10-15 00:20:41 -0500

    Added some validation and logging to console for username and password

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Form, Button} from "semantic-ui-react";
+import Validator from 'validator';
+import InlineError from '../messages/InlineError';
 
 class LoginForm extends React.Component {
   state = {
@@ -15,12 +17,27 @@ class LoginForm extends React.Component {
    this.setState({ data: {...this.state.data, [e.target.name]: e.target.value}
    });
 
+  onSubmit = () =>{
+    const errors = this.validate(this.state.data);
+    this.setState({ errors });
+    console.log(this.state.data.email);
+    console.log(this.state.data.password);
+  };
+
+  validate = (data) => {
+    const errors = {}
+    if (!Validator.isEmail(data.email)) errors.email = "Not A Valid Email";
+    if (!data.password) errors.password = "Password is empty";
+
+    return errors;
+  }
+
   render(){
-    const { data } = this.state
+    const { data, errors } = this.state
 
     return(
-      <Form>
-        <Form.Field>
+      <Form onSubmit={this.onSubmit}>
+        <Form.Field error={!!errors.email}>
           <label htmlFor="email">Email</label>
           <input
             type = "email"
@@ -30,8 +47,10 @@ class LoginForm extends React.Component {
             value = {data.email}
             onChange = {this.onChange}
           />
+          {errors.email && <InlineError text={errors.email} />}
         </Form.Field>
-        <Form.Field>
+
+        <Form.Field error={!!errors.password}>
           <label htmlFor="password">Password</label>
           <input
             type = "password"
@@ -41,6 +60,7 @@ class LoginForm extends React.Component {
             value = {data.password}
             onChange = {this.onChange}
           />
+          {errors.password && <InlineError text={errors.password} />}
         </Form.Field>
         <Button primary>Login</Button>
       </Form>
